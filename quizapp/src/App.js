af50535ba33d08cfe886dadf5b85c72c455f3689
Sudipto Das <dassudipto200@gmail.com>
2023-09-26 16:22:25 +0000

    compleing beta

--- quizapp/src/App.js
+++ quizapp/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { useState,useEffect } from 'react';
 const cat=[
@@ -25,7 +25,7 @@ function App() {
      <div className="rec-y"> 
       <div className="title">
         <div className='text'>Hello Kirat</div>
-        <img   style={{width:'40px',height:'40px',borderRadius:'50%'}} src="https://s3-alpha-sig.figma.com/img/89b4/446a/30abe92a0690905e1906270ebf9f799a?Expires=1696809600&Signature=PVsoX1F4NAhm8sdUTADUa18SEsusfpKR9XRZ7D-cRmMHZVrtYVZTlacRewowDNgI-hmFOWsmPMbOJsreK9fVeYdsuE0PaJnzisPdHZBwRKpdfS3FXM3rzPqTWBKCvMAu3Iz9aLGmrCA1xYtJPQnw77L~RewvqOZjyI12xDXcrG-tbfPaBCw8XQgyV5HCcXLI-QP~z5j8da4J-HAK3VoPASe5KlkAW0V1o1pGzDt9AUG0RFuBvxbp0UlbZo3PmnUvf5FSPjZFRrwvumtc60FMw5rYHszLHC8Cpotmbeo0qRuh5NzXfmDtvk3fRCeE4iEOTP8Bt6hxjM3uKltV6CAgKA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"/>
+        <img   style={{width:'40px',height:'40px',borderRadius:'50%'}}alt='dnd' src="https://s3-alpha-sig.figma.com/img/89b4/446a/30abe92a0690905e1906270ebf9f799a?Expires=1696809600&Signature=PVsoX1F4NAhm8sdUTADUa18SEsusfpKR9XRZ7D-cRmMHZVrtYVZTlacRewowDNgI-hmFOWsmPMbOJsreK9fVeYdsuE0PaJnzisPdHZBwRKpdfS3FXM3rzPqTWBKCvMAu3Iz9aLGmrCA1xYtJPQnw77L~RewvqOZjyI12xDXcrG-tbfPaBCw8XQgyV5HCcXLI-QP~z5j8da4J-HAK3VoPASe5KlkAW0V1o1pGzDt9AUG0RFuBvxbp0UlbZo3PmnUvf5FSPjZFRrwvumtc60FMw5rYHszLHC8Cpotmbeo0qRuh5NzXfmDtvk3fRCeE4iEOTP8Bt6hxjM3uKltV6CAgKA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"/>
       </div>
       <div className='text' style={{marginLeft:'20px',padding:'10px'}}>
         Popular
@@ -34,7 +34,7 @@ function App() {
       {cat.map((item)=>{
          return <div className='item'  style={{cursor:'pointer'}} onClick={()=>{setRoute('quiz');setCats(item.title)}} >
           <div  className='text-small' >{item.title}</div>
-          <img src={item.img} />
+          <img src={item.img} alt='aja' />
          </div>
       })}
       </div>
@@ -44,7 +44,7 @@ function App() {
      {allcat.map((item)=>{
          return <div className='item-2' style={{cursor:'pointer'}} onClick={()=>{setRoute('quiz')}}>
           <div  className='text-small' >{item.title}</div>
-          <img src={item.img} />
+          <img src={item.img} alt='dnhdh' />
          </div>
       })}
      </div>
@@ -54,16 +54,18 @@ function App() {
 function Quiz({ cat }) {
   const [data, setData] = useState({});
   const [API, setAPI] = useState("");
-  const [no, setNo] = useState(0);
-  const [border,setBorder]=useState("")
+const no =0;
+  
   const [myStyle, setMyStyle] = useState({});
+  const [route,setRoute]=useState("quiz")
+  
   const getCat = (cat) => {
     if (cat === "Sports") {
-      setAPI("https://opentdb.com/api.php?amount=10&category=21");
+      setAPI("https://opentdb.com/api.php?amount=1&category=21");
     } else if (cat === "Space") {
-      setAPI("https://opentdb.com/api.php?amount=10&category=17");
+      setAPI("https://opentdb.com/api.php?amount=1&category=17");
     } else {
-      setAPI("https://opentdb.com/api.php?amount=10&category=23");
+      setAPI("https://opentdb.com/api.php?amount=1&category=23");
     }
   };
   const handleClick = (id) => {
@@ -71,12 +73,11 @@ function Quiz({ cat }) {
       ...myStyle,
       [id]: !prevState[id],
     }));
-  
     setTimeout(() => {
-      let num = no;
-      num++;
-      setNo(num);
-    }, 3000); // Wait for 3 seconds (3000 milliseconds) before incrementing the number
+      setRoute("sc")
+    }, 3000); 
+   
+    // Wait for 3 seconds (3000 milliseconds) before incrementing the number
   };
   
   useEffect(() => {
@@ -95,7 +96,7 @@ function Quiz({ cat }) {
   }, [API, cat]);
 
   return (
-    <div className="App">
+    route ==="quiz"?<div className="App">
       <div className="rec-y">
         <div className="ques-div">
         <div
@@ -107,8 +108,13 @@ function Quiz({ cat }) {
                 position:'absolute',
                top:'40%',
                zIndex:1,
-                
+               justifyContent:'center',
+                fontSize:'20px',
+                display:'flex',
+                alignItems:'center',
+                fontWeight:'400pm',
                 background: "white",
+                border:"10px solid #FFB504"
               }}
             >
               {no}/20
@@ -141,16 +147,60 @@ function Quiz({ cat }) {
                  <div  className="option" id="cr-option" onClick={()=>{
                        document.getElementById("cr-option").style.borderColor="green";
                        document.getElementById("cr-option").style.borderWidth="5px";
-
                        setTimeout(() => {
-                        let num = no;
-                        num++;
-                        setNo(num);
-                      }, 3000);
+                        setRoute("sc")
+                      }, 3000); 
+                      
                     }}>{data.results && data.results[no] ? data.results[no].correct_answer : ""}</div> 
             </div>
-    </div>
+    </div>:<ScoreBoard/>
   );
+}
+function ScoreBoard(){
+return(<div className='App'>
+  <div className='rec-yx'>
+ <div className='my-score'  style={{
+                width: "80px",
+                height: "80px",
+                borderRadius: "50%",
+            margin:'auto',
+            position:'absolute',
+            top:'20%',
+  left:'45%',
+
+               justifyContent:'center',
+                fontSize:'20px',
+                display:'flex',
+                alignItems:'center',
+                fontWeight:'400pm',
+                background: "white",
+                border:"10px solid #FFB504"
+              }}><div>200 points</div></div>
+    <div className='ques-div'>
+      <div className='ques'>
+       <div className='st-row' style={{display:'flex',justifyContent:'space-evenly',gap:'20px',alignItems:'flex-start'}}>
+        <div>100% completion</div> 
+        <div>20 complete</div></div>
+        <div className='st-row' style={{display:'flex',justifyContent:'space-evenly',gap:'20px',alignItems:'flex-end'}}>
+        <div style={{color:'green'}}>10 correct</div> 
+        <div style={{color:'red'}}>7 wrong</div></div>
+      </div>
+      
+    </div>
+  </div>
+  <div className='explore'> {[{title:"Home",img:"./G1.png"},
+  {title:"Leader Board",img:"./G2.png"},
+  {title:"Share",img:"./G3.png"},
+  {title:"Home",img:"./G1.png"},
+  {title:"Leader Board",img:"./G2.png"},
+  {title:"Share",img:"./G3.png"}].map((item)=>{
+         return <div className='item-2' style={{cursor:'pointer'}} >
+          <div  className='text-small' >{item.title}</div>
+          <img src={item.img} alt='djdj' />
+         </div>
+      })}</div>
+ 
+</div>)
 }
 
 
